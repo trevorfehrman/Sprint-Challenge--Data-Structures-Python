7079cb1534f1512088e2d2c6a4c71f5cc892f4da
Trevor Fehrman <trevorfehrman@gmail.com>
2019-05-10 09:34:19 -0700

    Mvp

--- names/names.py
+++ names/names.py
@@ -1,5 +1,55 @@
 import time
 
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        # if this is the bottom of the tree, and both nodes are empty, insert accordingly
+        if self.value == None:
+            self.value == value
+        elif value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left != None:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if self.right != None:
+                return self.right.contains(target)
+            else:
+                return False
+
+    def get_max(self):
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,13 +60,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+bts_names_2 = BinarySearchTree(names_2[0])
+
+for name_1 in names_2[1:]:
+    bts_names_2.insert(name_1)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# for name_1 in names_1:
+#     if bts_names_2.contains(name_1):
+#         duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,19 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        self.storage[self.current] = item
+        if self.current == self.capacity - 1:
+            self.current = 0
+        else:
+            self.current += 1
 
-  def get(self):
-    pass
+    def get(self):
+        temp = []
+        for i in self.storage:
+            if i != None:
+                temp.append(i)
+        return temp
